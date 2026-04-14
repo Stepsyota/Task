9a60b28b1c375e1a3cefc64837314fa8d56bf2a7
Stepan <stepsyota@gmail.com>
2026-04-14 14:17:39 +0300

    ADD: usage of Config, ConfigParser and ConfigValidator

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include "config.h"
 
-int main() {
-    std::cout << "Hello World\n";
+int main(int argc, char* argv[]) {
+    Config cfg;
+
+    if (!ConfigParser::parse(argc, argv,cfg)) {
+        return 1;
+    }
+
+    if (!ConfigValidator::validate(cfg)) {
+        return 1;
+    }
+
+
+    std::cout << cfg.scan_directory << std::endl;
+    std::cout << cfg.white_list_file << std::endl;
+    std::cout << cfg.black_list_file << std::endl;
+    std::cout << cfg.extensions_file << std::endl;
+
     return 0;
 }
