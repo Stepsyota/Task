f55d5f969db153cc9c5cce0ab1607603dbe14346
Stepan <stepsyota@gmail.com>
2026-04-22 13:02:36 +0300

    FIX: the structure has been rewritten, readability and clarity are better, comments have been added

--- src/app/agent.cpp
+++ src/app/agent.cpp
@@ -2,23 +2,32 @@
 #include "../scan/directory_scanner.h"
 #include "../core/file_processor.h"
 #include "../io/values_reader.h"
-#include "io/info_writer.h"
+#include "../io/info_writer.h"
 
 
-Agent::Agent(Config cfg) : config(cfg), writer("output_white.txt", "output_black.txt", "output_anomaly.txt"){}
+Agent::Agent(Config cfg) : config(cfg), 
+    writer(cfg.get_white_output_file(),
+        cfg.get_black_output_file(),
+        cfg.get_anomaly_output_file()) {
+            
+        white_hash_set = ValuesReader::load_hashes_to_set(cfg.get_white_list_file());
+        black_hash_set = ValuesReader::load_hashes_to_set(cfg.get_black_list_file());
+        extensions_set = ValuesReader::load_extensions_to_set(cfg.get_extensions_file());
+    }
 
 void Agent::run() {
-    load_sets();
-
     auto info = InfoWriter::collect(config.get_scan_directory());
     InfoWriter::write(info);
 
-    FileProcessor processor(white_hash_set, black_hash_set, extensions_set, writer);
-    DirectoryScanner::scan(config.get_scan_directory(), processor);
-}
+    FileProcessor processor(
+        white_hash_set,
+        black_hash_set,
+        extensions_set
+    );
 
-void Agent::load_sets() {
-    white_hash_set = ValuesReader::load_hashes_to_set(config.get_white_list_file());
-    black_hash_set = ValuesReader::load_hashes_to_set(config.get_black_list_file());
-    extensions_set = ValuesReader::load_extensions_to_set(config.get_extensions_file());
+    DirectoryScanner scanner;
+    scanner.scan(config.get_scan_directory(), [&](const std::filesystem::path & file) {
+        const auto result = processor.process(file);
+        writer.write(result);
+    });
 }
